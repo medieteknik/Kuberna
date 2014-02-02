b3914fb0c132ec2c2c4d2adab0d8accbe55f2289
Klas Eskilson <klas.eskilson@gmail.com>
2014-02-02 18:58:19 +0100

    Removed ugly redir

--- vote.php
+++ vote.php
@@ -13,11 +13,13 @@ $gray = mysqli_real_escape_string($sysdb, $_POST['gray']);
 $orange = mysqli_real_escape_string($sysdb, $_POST['orange']);
 $liuid = mysqli_real_escape_string($sysdb, phpCAS::getUser());
 
+// prepare query
 $query = "INSERT INTO votes (gray, orange, liuid) VALUES('$gray', '$orange', '$liuid')";
 
-if(!has_voted(phpCAS::getUser(), $sysdb) && $sysdb->query($query))
-{
-	header('Location: ./?login#black');
-}
-else
-	header('Location: ./?login#black');
+// has the user voted before?
+if(!has_voted(phpCAS::getUser(), $sysdb))
+	// run query
+	$sysdb->query($query);
+
+// redir to index
+header('Location: ./#black');
