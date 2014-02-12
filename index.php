8a731058aecb669912cacd82c32ba97ecfcd109b
Klas Eskilson <klas.eskilson@gmail.com>
2014-02-12 11:53:03 +0100

    Added google analytics

--- index.php
+++ index.php
@@ -332,5 +332,14 @@ $data = json_decode($datafile, true);
 					</div>
 				</div>
 			</div>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-47996085-1', 'medieteknik.nu');
+			ga('send', 'pageview');
+		</script>
 	</body>
 </html>
